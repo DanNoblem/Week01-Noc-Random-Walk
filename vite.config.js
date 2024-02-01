f0c6b58558f7776c7912fe9e3600e878f7dc58a1
DanNoble <danielwai999@gmail.com>
2024-02-01 13:03:53 -0500

    building Page

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: resolve(__dirname, "src"),
+  server: {
+    port: 3000,
+  },
+  base: "/RandomWalk/",
+  build: {
+    outDir: "../docs",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+      },
+    },
+  },
+  publicDir: resolve(__dirname, "public"),
+});
